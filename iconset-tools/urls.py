b41fd11986f5a775e87b5a861ea09d75832f5616
Vid <vidmantas.zemleris@gmail.com>
2012-01-14 22:31:23 +0100

    iconset-tools updated

--- iconset-tools/urls.py
+++ iconset-tools/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    (r'^', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
